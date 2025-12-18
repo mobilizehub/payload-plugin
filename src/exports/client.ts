38a163e5a8c5b1fdc4af5be5728d88ca117b0914
Ryan Mearns <ryan.j.mearns@gmail.com>
2025-12-19 08:17:35 +1300

    Integrate broadcast system into plugin exports
    - Add broadcasts, emails, and unsubscribe-tokens collections to plugin
    - Register sendEmail and sendBroadcasts background tasks
    - Add API endpoints for broadcast operations
    - Export types for EmailAdapter, EmailMessage, and plugin config
    - Export client and RSC components for admin UI

---
+++ src/exports/client.ts
@@ -0,0 +1,3 @@
+export { SendBroadcastModal } from '../components/broadcast-send-modal.js'
+export { SendTestBroadcastDrawer } from '../components/broadcast-send-test-drawer.js'
+export { EmailPreviewField } from '../components/email-preview.js'
