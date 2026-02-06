182e7fb93ed5a0a2ddd09cf0396504af778a8666
Ryan Mearns <ryan.j.mearns@gmail.com>
2026-02-07 09:14:02 +1300

    feat: export field creators via @mobilizehub/payload-plugin/fields

---
+++ src/exports/fields.ts
@@ -0,0 +1,4 @@
+export { createNameField } from '../fields/name.js'
+export { createPublishedAtField } from '../fields/publishedAt.js'
+export { createSlugField } from '../fields/slug.js'
+export { createStatusField } from '../fields/status.js'
